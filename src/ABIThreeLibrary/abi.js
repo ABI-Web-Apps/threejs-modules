0288a3dcd1759f439ac466cab76a162e7f7a4106
LinkunGao <gaolinkun123@gmail.com>
2021-12-21 10:34:30 +1300

    build grid for multiple scenes, create render function

--- src/ABIThreeLibrary/abi.js
+++ src/ABIThreeLibrary/abi.js
@@ -62,4 +62,44 @@ class ABIThree {
       this.renderer.render(scene, camera);
     };
   }
+  resizeRendererToDisplaySize = () => {
+    const width = this.renderer.domElement.clientWidth;
+    const height = this.renderer.domElement.clientHeight;
+    const needResize =
+      this.renderer.domElement.width !== width ||
+      this.renderer.domElement.height !== height;
+    if (needResize) {
+      // to create a grid for multiple scenes
+      this.elems.map((elem, index) => {
+        if (index === this.sceneNum - 1 && this.sceneNum % 2 !== 0) {
+          elem.style.width = this.container.clientWidth + "px";
+        } else {
+          elem.style.width = this.container.clientWidth / 2 - 2 + "px";
+        }
+        elem.style.height =
+          this.container.clientHeight / Math.ceil(this.sceneNum / 2) + "px";
+      });
+      this.renderer.setSize(width, height, false);
+    }
+  };
+  animate = (time) => {
+    time *= 0.001;
+    const clearColor = new THREE.Color("#000");
+    this.renderer.setScissorTest(false);
+    this.renderer.setClearColor(clearColor, 0);
+    this.renderer.clear(true, true);
+    this.setScissorTest(true);
+    this.resizeRendererToDisplaySize();
+
+    for (let info of this.sceneInfos) {
+      this.renderSceneInfo(info);
+    }
+
+    const transform = `translateY(${window.scrollY}px)`;
+    this.renderer.domElement.style.transform = transform;
+
+    window.requestAnimationFrame(this.animate);
+  };
 }
+
+export { ABIThree, ImageLoader, createTestMesh };
